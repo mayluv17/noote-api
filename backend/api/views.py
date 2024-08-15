9955b8467934689c7fe3be1cbe059e6027c43baf
“mayluv17” <mayowaibrahim009@gmail.com>
2024-08-15 10:56:40 +0100

    add note CRUD

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, NoteSerializer
